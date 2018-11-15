9eb251db77b986d5f48b97163f5201f2590c3590
kenny herillard <Herillard>
2018-11-15 14:48:00 +0100

    Add interaction with console

--- Console.js
+++ Console.js
@@ -1,14 +1,52 @@
-process.openStdin().addListener("data", d => {
-    console.log("You entered: [" + d.toString().trim() + "]");
+function SlowLog(texte,time,suite){
+    let lettreCourante = 0;
+    for( let i = 0; i < texte.length + 1; i++){
+        setTimeout(() =>{
+            if( i < texte.length){
+                process.stdout.write(texte[lettreCourante]);
+                lettreCourante++;
+            }else{
+                suite();
+            }
+        }, i*time);
+    }
+}
+
+let intro = 
+`Bienvenue dans le désert de la mort ... ☼
+Partout des ossements de chameaux morts et une odeur de 
+putréfaction.
+`;
+
+let suite = 
+`Maintenant, vous entrez dans la caverne ...
+`
+
+//On précise que nous taperons de l'utf 8 dans la console
+process.stdin.setEncoding('utf8');
+
+//A chaque entrée dans la console, on appellera la fonction fléchée, rep sera la réponse tapée dans la console
+//Ici on déclare ce que l'on fera lorsqu'on recevra une donée
+process.stdin.on('data', (d) => {	
+	let rep = d.toString().trim() 
+	if(rep == "suite"){
+		process.stdin.pause(); //stopper l'entrée
+		SlowLog(suite,100, () =>{
+			SlowLog("on fait quoi 2 ?\n",100, () => {
+				process.stdin.resume();//réactiver l'entrée à la fin du log
+			});
+		});
+	}
+	if(rep == "quit"){
+		process.exit();
+	}
 });
 
-function Demande(question) {
-    let reponse;
-// afficher la réponse
-    console.log(question);
-// chopper la réponse
-    process.openStdin.addListener("data", d => {
-        return d.toString().trim();
-    });
-    return reponse;
-}
+
+/* début du script ici */
+process.stdin.pause(); //stopper l'entrée pour ne pas pirater le texte
+SlowLog(intro,100,()=> {    
+    SlowLog("on fait quoi ?\n",100, () => {
+		process.stdin.resume();//réactiver l'entrée
+	} );
+});
